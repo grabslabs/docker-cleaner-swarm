9d4434701dded794e2482bf4216c72b62aa4cb24
senzacionale <mitja.bombac@gmail.com>
2024-08-01 08:16:06 +0200

    [FIX] You may want to avoid this part to prevent accidental removal of untagged but necessary images. Only enable if you're sure they are safe to delete.

--- image/app/images.sh
+++ image/app/images.sh
@@ -7,7 +7,8 @@ if docker ps -qa --filter "status=exited" | grep -q . ; then
     # Remove dangling images
     docker rmi $(docker images --filter "dangling=true" -q --no-trunc) ||:
     # Remove images with name "none"
-    docker rmi $(docker images | grep "none" | awk '/ / { print $3 }') ||:
+    # You may want to avoid this part to prevent accidental removal of untagged but necessary images. Only enable if you're sure they are safe to delete.
+    # docker rmi $(docker images | grep "none" | awk '/ / { print $3 }') ||:
 else
     echo "No exited images to remove."
 fi
